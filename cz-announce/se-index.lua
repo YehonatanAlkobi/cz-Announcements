fe5aa6e67bea0fec3e31df6470dbc2f6e2ccce0e
chickZ <61382579+chickXYZ@users.noreply.github.com>
2022-01-12 00:22:11 +0200

    Update se-index.lua

--- cz-announce/se-index.lua
+++ cz-announce/se-index.lua
@@ -3,7 +3,7 @@ local QBCore = exports['qb-core']:GetCoreObject();
 RegisterCommand("notify", function(source, args)
     for k,v in pairs(GetPlayers()) do
         if QBCore.Functions.HasPermission(v, "admin") then
-            if args ~= nil then
+            if args == " " or "" then
                 TriggerClientEvent("cz-announce:toggleDisplay", -1, tostring(table.concat(args, " ")), 7000)
             else
                 TriggerClientEvent("cz-announce:toggleDisplay", source, "need to add alittle bit more words buddy", 2000)
